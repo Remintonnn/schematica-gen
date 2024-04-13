806184750527df8e19a6bab2ac627c1001257958
MSI\a1187 <a1187731600@gmail.com>
2024-04-14 02:08:13 +0800

    implemented the initial part of the schem generation

--- main.py
+++ main.py
@@ -16,13 +16,27 @@ def saveSettings():
     pass
 
 def build(song):
+    # song.header.song_layers is unrelyable as it won't go down after it went up
+    # a.k.a won't react to manual song compression 
+    layerNeed = 0
     for tick, chord in song:
-        print(tick, [f"{settings.get('instruments')[note.instrument].get('id')}:{note.key-baseKey}" for note in chord])
+        for note in chord:
+            if note.layer>layerNeed: layerNeed=note.layer
+    print(layerNeed)
+    # for tick, chord in song:
+    #     print(tick, [f"{settings.get('instruments')[note.instrument].get('id')}:{note.key-baseKey}" for note in chord])
+    return
     # schem = mcschematic.MCSchematic()
 
     # schem.setBlock(  (0, -1, 0), "minecraft:stone"  )
 
     # schem.save( "", "output", mcschematic.Version.JE_1_18_2,False)
+    def buildHead(layer):
+        pass
+    def module(layer):
+        pass
+    def turn(layer):
+        pass
 
 def init():
     getSettings()
